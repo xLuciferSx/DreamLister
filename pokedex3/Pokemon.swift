4e85ec9c4f3361b68ee28844dc28b6fecf84c091
Raivis Olehno <21298759@student.uwl.ac.uk>
2017-09-06 17:46:28 +0100

    Working on ViewController and creating base UI

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Raivis Olehno on 06/09/2017.
+//  Copyright © 2017 Raivis Olehno. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init (name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
