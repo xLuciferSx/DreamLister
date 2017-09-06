1edd04b5186e3ee01955a1467aa2e6461cea987d
Raivis Olehno <21298759@student.uwl.ac.uk>
2017-09-06 17:53:15 +0100

    Created a file for pokemons to store pokemon data

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Raivis Olehno on 06/09/2017.
+//  Copyright © 2017 Raivis Olehno. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
